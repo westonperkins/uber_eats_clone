a34714ef37de8fa0b37f77ff0f922534b8f62e68
westonperkins <westonperkins5@gmail.com>
2021-11-09 13:28:11 -0500

    categories and search

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
-import HeaderTabs from '../components/HeaderTabs'
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import Categories from "../components/Categories";
+import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
-    return (
-        <SafeAreaView>
-            <HeaderTabs/>
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView style={{backgroundColor: '#eee', flex: 1}}>
+      <View style={{backgroundColor: 'white', padding: 15}}>
+        <HeaderTabs />
+        <SearchBar/>
+      </View>
+      <Categories/>
+    </SafeAreaView>
+  );
 }
