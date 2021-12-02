2a6ebd85ad8a91dc45e47e2eefe343e55071159d
westonperkins <westonperkins5@gmail.com>
2021-12-02 13:42:11 -0500

    updated menu items

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -7,41 +7,41 @@ import ViewCart from "../components/RestaurantDetail/ViewCart";
 
 const foods = [
   {
-    title: "Lasagna",
-    description: "With butter lettuce, tomato and sauce bechamel",
+    title: "Hamburger with fries",
+    description: "Hamburger with lettuce tomato onions pickels with the option of cheese. Fries included.",
     price: "$13.50",
     image:
-      "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
+      "https://www.aspicyperspective.com/wp-content/uploads/2020/07/best-hamburger-patties-1.jpg",
   },
   {
-    title: "Tandoori Chicken",
+    title: "Hot Dog",
     description:
-      "Amazing Indian dish with tenderloin chicken off the sizzles 🔥",
+      "All beef hot dog with optional ketchup, mustard, mayo, and relish.",
     price: "$19.20",
-    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
+    image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F35%2F2021%2F08%2F24%2Fhotdog-GettyImages-1163965971.jpg&q=85",
   },
   {
-    title: "Chilaquiles",
+    title: "BLTA",
     description:
-      "Chilaquiles with cheese and sauce. A delicious mexican dish 🇲🇽",
+      "Classic bacon lettuce and tomato sandwich with avocado. Serverd on white bread.",
     price: "$14.50",
     image:
-      "https://i2.wp.com/chilipeppermadness.com/wp-content/uploads/2020/11/Chilaquales-Recipe-Chilaquiles-Rojos-1.jpg",
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-200511-seo-club-sandwich-h-14383-eb-1590780714.jpg?crop=0.667xw:1.00xh;0.109xw,0&resize=640:*",
   },
   {
     title: "Chicken Caesar Salad",
     description:
-      "One can never go wrong with a chicken caesar salad. Healthy option with greens and proteins!",
+      "One can never go wrong with a chicken caesar salad. Healthy option with greens and proteins.",
     price: "$21.50",
     image:
       "https://images.themodernproper.com/billowy-turkey/production/posts/2019/Easy-italian-salad-recipe-10.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1614096227&s=c0f63a30cef3334d97f9ecad14be51da",
   },
   {
-    title: "Lasagna",
-    description: "With butter lettuce, tomato and sauce bechamel",
+    title: "Spaghetti Bolognese",
+    description: "Spaghetti with an Italian ragù made with minced beef, bacon and tomatoes, served with Parmesan cheese.",
     price: "$13.50",
     image:
-      "https://thestayathomechef.com/wp-content/uploads/2017/08/Most-Amazing-Lasagna-2-e1574792735811.jpg",
+      "https://www.errenskitchen.com/wp-content/uploads/2015/02/Quick-Easy-Spaghetti-Bolognese2-1-500x480.jpg",
   },
 ];
 
